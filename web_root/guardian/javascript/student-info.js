fb25ce9687b660abc1d8cc7ede6bd94f7aa7f171
Nathan Jones <nathan.jones@ironmail.org>
2014-08-14 19:05:44 -0600

    Made progress on getting student info changes to work.

--- web_root/guardian/javascript/student-info.js
+++ web_root/guardian/javascript/student-info.js
@@ -1,6 +1,6 @@
 /*global $*/
 
-$(document).ready(function () {
+$j(document).ready(function () {
     var config = {
         // PowerSchool Point of Contact server, which will talk with the PS REST API.
         "psPoc": "https://psit.irondistrict.org",
@@ -9,23 +9,38 @@ $(document).ready(function () {
         "stuInfoDbe": "u_student_info"
     };
 
+    var studentInInfoStaging;
+    $j.get(config['psPoc'] + '/student-info/staging/' + psData.studentDcid, function (resp) {
+
+        // Check if student has staging record
+        if (resp.record.length === 0) {
+            studentInInfoStaging = false;
+        } else {
+            studentInInfoStaging = true;
+        }
+
+        if (studentInInfoStaging) {
+            $j('#staging-on-file').css({'display': 'block'});
+        }
+    }, 'json');
+
     // Load on-file data into form.
-    $.get(config['psPoc'] + '/student-info/' + psData.studentDcid, function (resp) {
+    $j.get(config['psPoc'] + '/student-info/' + psData.studentDcid, function (resp) {
         var studentInfo = resp.student;
-        $('#on-file-mailing-street').text(studentInfo.addresses.mailing.street);
-        $('#on-file-mailing-city').text(studentInfo.addresses.mailing.city);
-        $('#on-file-mailing-state').text(studentInfo.addresses.mailing.state_province);
-        $('#on-file-mailing-zip').text(studentInfo.addresses.mailing.postal_code);
+        $j('#on-file-mailing-street').text(studentInfo.addresses.mailing.street);
+        $j('#on-file-mailing-city').text(studentInfo.addresses.mailing.city);
+        $j('#on-file-mailing-state').text(studentInfo.addresses.mailing.state_province);
+        $j('#on-file-mailing-zip').text(studentInfo.addresses.mailing.postal_code);
 
-        $('#on-file-residence-street').text(studentInfo.addresses.physical.street);
-        $('#on-file-residence-city').text(studentInfo.addresses.physical.city);
-        $('#on-file-residence-state').text(studentInfo.addresses.physical.state_province);
-        $('#on-file-residence-zip').text(studentInfo.addresses.physical.postal_code);
+        $j('#on-file-residence-street').text(studentInfo.addresses.physical.street);
+        $j('#on-file-residence-city').text(studentInfo.addresses.physical.city);
+        $j('#on-file-residence-state').text(studentInfo.addresses.physical.state_province);
+        $j('#on-file-residence-zip').text(studentInfo.addresses.physical.postal_code);
 
-        $('#on-file-student-email').text(studentInfo.contact_info.email);
+        $j('#on-file-student-email').text(studentInfo.contact_info.email);
 
         // Find the students_extension object
-        var studentsExtension = $.grep(studentInfo._extension_data._table_extension, function (elem) {
+        var studentsExtension = $j.grep(studentInfo._extension_data._table_extension, function (elem) {
             if (elem.name === 'u_students_extension') {
                 return elem._field;
             }
@@ -35,7 +50,7 @@ $(document).ready(function () {
         // If there is more than one field in the U_STUDENTS_EXTENSION table, this value is an array
         var studentCellPhone;
         if (Object.prototype.toString.call(studentsExtension._field) === '[object Array]') {
-            studentCellPhone = $.grep(studentsExtension, function (elem) {
+            studentCellPhone = $j.grep(studentsExtension, function (elem) {
                 if (elem.name === 'student_cell_number') {
                     return elem;
                 }
@@ -46,26 +61,26 @@ $(document).ready(function () {
         }
 
 
-        $('#on-file-student-cell-phone').text(studentCellPhone.value);
+        $j('#on-file-student-cell-phone').text(studentCellPhone.value);
 
-        $('#on-file-doctor-name').text(studentInfo.contact.doctor_name);
-        $('#on-file-doctor-phone').text(studentInfo.contact.doctor_phone);
+        $j('#on-file-doctor-name').text(studentInfo.contact.doctor_name);
+        $j('#on-file-doctor-phone').text(studentInfo.contact.doctor_phone);
 
 
-        var studentCoreFieldsExt = $.grep(studentInfo._extension_data._table_extension, function (elem) {
+        var studentCoreFieldsExt = $j.grep(studentInfo._extension_data._table_extension, function (elem) {
             if (elem.name === 'studentcorefields') {
                 return elem._field;
             }
         });
         studentCoreFieldsExt = studentCoreFieldsExt[0]._field;
 
-        var dentist_name = $.grep(studentCoreFieldsExt, function (elem) {
+        var dentist_name = $j.grep(studentCoreFieldsExt, function (elem) {
             if (elem.name === 'dentist_name') {
                 return elem;
             }
         });
 
-        var dentist_phone = $.grep(studentCoreFieldsExt, function (elem) {
+        var dentist_phone = $j.grep(studentCoreFieldsExt, function (elem) {
             if (elem.name === 'dentist_phone') {
                 return elem;
             }
@@ -74,56 +89,53 @@ $(document).ready(function () {
         dentist_name = dentist_name[0];
         dentist_phone = dentist_phone[0];
 
-        $('#on-file-dentist-name').text(dentist_name.value);
-        $('#on-file-dentist-phone').text(dentist_phone.value);
+        $j('#on-file-dentist-name').text(dentist_name.value);
+        $j('#on-file-dentist-phone').text(dentist_phone.value);
     }, 'json');
 
-    $('#demographic-update-form').on('submit', function (e) {
+
+    $j('#demographic-update-form').on('submit', function (e) {
         e.preventDefault();
         e.stopPropagation();
 
-        //Check if student already has student info record in staging_info_staging table
-        $.get(config['psPoc'] + '/student-info/staging/' + psData.studentDcid, function (resp) {
+        var postData = {
+            'mailing-street': $j('#mailing-street').val(),
+            'mailing-city': $j('#mailing-city').val(),
+            'mailing-state': $j('#mailing-state').val(),
+            'mailing-zip': $j('#mailing-zip').val(),
+            'residence-street': $j('#residence-street').val(),
+            'residence-city': $j('#residence-city').val(),
+            'residence-state': $j('#residence-state').val(),
+            'residence-zip': $j('#residence-zip').val(),
+            'student-email': $j('#student-email').val(),
+            'student-cell-phone': $j('#student-cell-phone').val(),
+            'doctor-name': $j('#doctor-name').val(),
+            'doctor-phone': $j('#doctor-phone').val(),
+            'dentist-name': $j('#dentist-name').val(),
+            'dentist-phone': $j('#dentist-phone').val()
+        };
 
-            var postData = {
-                'mailing-street': $('#mailing-street').val(),
-                'mailing-city': $('#mailing-city').val(),
-                'mailing-state': $('#mailing-state').val(),
-                'mailing-zip': $('#mailing-zip').val(),
-                'residence-street': $('#residence-street').val(),
-                'residence-city': $('#residence-city').val(),
-                'residence-state': $('#residence-state').val(),
-                'residence-zip': $('#residence-zip').val(),
-                'student-email': $('#student-email').val(),
-                'student-cell-phone': $('#student-cell-phone').val(),
-                'doctor-name': $('#doctor-name').val(),
-                'doctor-phone': $('#doctor-phone').val(),
-                'dentist-name': $('#dentist-name').val(),
-                'dentist-phone': $('#dentist-phone').val()
-            };
-
-            // This student doesn't have a staging table record, so make a new one.
-            if (resp.record.length === 0) {
-                $.ajax({
-                    url: config['psPoc'] + '/student-info/staging/new/' + psData.studentDcid,
-                    type: "POST",
-                    dataType: "json",
-                    data: postData
-                }).done(function (resp) {
-                    console.dir(resp);
-                });
-            } else {
-                // This student already has a staging table record, so update it
-                var stagingId = resp.record[0].id;
-                $.ajax({
-                    url: config['psPoc'] + '/student-info/staging/update/' + stagingId,
-                    type: "POST",
-                    dataType: "json",
-                    data: postData
-                }).done(function (resp) {
-                    console.dir(resp)
-                });
-            }
-        }, 'json');
+        // This student doesn't have a staging table record, so make a new one.
+        if (!studentInInfoStaging) {
+            $j.ajax({
+                url: config['psPoc'] + '/student-info/staging/new/' + psData.studentDcid,
+                type: "POST",
+                dataType: "json",
+                data: postData
+            }).done(function (resp) {
+                console.dir(resp);
+            });
+        } else {
+            // This student already has a staging table record, so update it
+            var stagingId = resp.record[0].id;
+            $j.ajax({
+                url: config['psPoc'] + '/student-info/staging/update/' + stagingId,
+                type: "POST",
+                dataType: "json",
+                data: postData
+            }).done(function (resp) {
+                console.dir(resp)
+            });
+        }
     });
 });
