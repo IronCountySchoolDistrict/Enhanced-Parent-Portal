d48647b39eea6f7e3131e2309d09d09ce94eb465
Nathan Jones <nathan.jones@ironmail.org>
2014-10-02 08:38:03 -0600

    Added a JS module that holds config options.

---
+++ web_root/scripts/student-contacts-update/config.js
@@ -0,0 +1,11 @@
+/*global define*/
+define(function() {
+    'use strict';
+    return {
+        'psApi': 'https://psapitest.irondistrict.org',
+
+        'studentContactsTable': 'U_STUDENT_CONTACTS3',
+
+        'studentContactsStagingTable': 'U_STUDENT_CONTACTS_STAGING3'
+    };
+});
