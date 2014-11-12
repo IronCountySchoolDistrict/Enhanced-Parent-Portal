33b1d003259fc8fd97b608558a226c88e3498451
Nathan Jones <nathan.jones@ironmail.org>
2014-11-11 17:28:41 -0700

    Added README, hide edit, add buttons so more than one add/edit form can't be shown

--- web_root/scripts/student-contacts-update/table-module.js
+++ web_root/scripts/student-contacts-update/table-module.js
@@ -7,6 +7,7 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
             this.addContactButton();
             this.bindEditButton();
             this.bindAddButton();
+            this.setupParsley();
         },
 
         /**
@@ -17,7 +18,7 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
          */
         _getAllContactPriorities: function (contactRows) {
             var allPriorities = [];
-            _.each(contactRows, function(contactRow) {
+            _.each(contactRows, function (contactRow) {
                 var rowData = $j(contactRow).data();
                 var priorityInt = Number.parseInt(rowData.contactData.priority);
                 allPriorities.push(priorityInt);
@@ -33,7 +34,7 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
          */
         _getAllContactIds: function (contactRows) {
             var allContactIds = [];
-            _.each(contactRows, function(contactRow) {
+            _.each(contactRows, function (contactRow) {
                 var rowData = $j(contactRow).data();
                 var priorityInt = Number.parseInt(rowData.contactData.contact_id);
                 allContactIds.push(priorityInt);
@@ -56,8 +57,8 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
             /**
              * @see sDom option in dataTable() initialization.
              */
-            $j('#parents-guardians-content').prepend('<button id="add-par-guar-contact">Add Contact</button>');
-            $j('#emergency-contacts-content').prepend('<button id="add-emerg-contact">Add Contact</button>');
+            $j('#parents-guardians-content').prepend('<button id="add-par-guar-contact" class="add-cont-btn">Add Contact</button>');
+            $j('#emergency-contacts-content').prepend('<button id="add-emerg-contact" class="add-cont-btn">Add Contact</button>');
 
             $j('#parents-guardians-content button, #emergency-contacts-content button').button({
                 icons: {
@@ -73,6 +74,10 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
             $j('body').on('click', '.editcontact', function (event) {
                 var $eventTarget = $j(event.target);
                 var $parentRow = $eventTarget.closest('tr');
+
+                $eventTarget.parents('.contacts-content').find('.editcontact').hide();
+                $eventTarget.parents('.contacts-content').find('.add-cont-btn').hide();
+
                 var isParGuarContact = $parentRow.closest('#parents-guardians-table').length > 0;
 
                 var contactData = $parentRow.data('contactData');
@@ -87,9 +92,9 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
          */
         bindSaveButton: function (isParGuarContact) {
             var _this = this;
-            $j('.editcontact').show();
             $j('.savecontact').on('click', function (event) {
-
+                $eventTarget.parents('.contacts-content').find('.editcontact').show();
+                $eventTarget.parents('.contacts-content').find('.add-cont-btn').show();
                 var $eventTarget = $j(event.target);
 
                 var $closestRow = $eventTarget.closest('tr');
@@ -143,7 +148,7 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
                             contactData.id = contactInitData.id;
                         } else {
                             contactData.contact_id = newContactId;
-                            contactData.id =  resp.result[0].success_message.id;
+                            contactData.id = resp.result[0].success_message.id;
                         }
 
                         actions.renderContact(contactData, $closestRow, true);
@@ -154,10 +159,10 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
 
         bindAddButton: function () {
             var _this = this;
-            $j('#add-par-guar-contact, #add-emerg-contact').on('click', function(event) {
+            $j('#add-par-guar-contact, #add-emerg-contact').on('click', function (event) {
                 var allPriorities = _this._getAllContactPriorities(_this._getAllContactRows());
                 var $target = $j(event.target);
-                var $targetButton =  $target.closest('button');
+                var $targetButton = $target.closest('button');
                 $targetButton.css({'display': 'none'});
                 var addParGuar = $targetButton.attr('id') === 'add-par-guar-contact';
                 var buttonTable = addParGuar ? '#parents-guardians-table' : '#emergency-contacts-table';
@@ -166,6 +171,154 @@ define(['actions', 'service', 'underscore'], function (actions, service, _) {
                 newRow.insertAfter(insertSelector);
                 actions.addContact(newRow, addParGuar, allPriorities);
                 _this.bindSaveButton(addParGuar);
+            });
+        },
+
+        setupParsley: function () {
+            window.ParsleyValidator
+                .addValidator('resaddress', function (value) {
+                    /**
+                     *
+                     * @type {boolean}
+                     */
+                    var resFieldsEmpty = $('#residence-street').val() === "" &&
+                        $('#residence-city').val() === "" &&
+                        $('#residence-state').val() === "" &&
+                        $('#residence-zip').val() === "";
+                    if (resFieldsEmpty) {
+                        return true;
+                    } else {
+                        return !!value;
+                    }
+
+                }, 100)
+                .addMessage('en', 'resaddress', 'All address fields must be filled in');
+
+            window.ParsleyValidator
+                .addValidator('mailaddress', function (value) {
+                    /**
+                     *
+                     * @type {boolean}
+                     */
+                    var mailFieldsEmpty = $('#mailing-street').val() === "" &&
+                        $('#mailing-city').val() === "" &&
+                        $('#mailing-state').val() === "" &&
+                        $('#mailing-zip').val() === "";
+                    if (mailFieldsEmpty) {
+                        return true;
+                    } else {
+                        return !!value;
+                    }
+
+                }, 100)
+                .addMessage('en', 'mailaddress', 'All address fields must be filled in');
+
+            window.ParsleyValidator
+                .addValidator('onephonereq', function (value) {
+                    /**
+                     *
+                     * @type {boolean}
+                     */
+                    var allPhonesEmpty = $('#phone1type').val() === "" &&
+                        $('#phone1').val() === "" &&
+                        $('#phone2type').val() === "" &&
+                        $('#phone2').val() === "" &&
+                        $('#phone3type').val() === "" &&
+                        $('#phone3').val() === "";
+
+                    if (allPhonesEmpty) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+
+                }, 100)
+                .addMessage('en', 'onephonereq', 'At least one phone number is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone1num', function (value) {
+                    if ($('#phone1type').val() === "" && $('#phone1').val() === "") {
+                        return true;
+                    } else if ($('#phone1type').val() !== "" && $('#phone1').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone1num', 'Phone type was given, number is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone1type', function (value) {
+                    if ($('#phone1type').val() === "" && $('#phone1').val() === "") {
+                        return true;
+                    } else if ($('#phone1').val() !== "" && $('#phone1type').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone1type', 'Phone number was given, type is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone2num', function (value) {
+                    if ($('#phone2type').val() === "" && $('#phone2').val() === "") {
+                        return true;
+                    } else if ($('#phone2type').val() !== "" && $('#phone2').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone2num', 'Phone type was given, number is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone2type', function (value) {
+                    if ($('#phone2type').val() === "" && $('#phone2').val() === "") {
+                        return true;
+                    } else if ($('#phone2').val() !== "" && $('#phone2type').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone2type', 'Phone number was given, type is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone3num', function (value) {
+                    if ($('#phone3type').val() === "" && $('#phone3').val() === "") {
+                        return true;
+                    } else if ($('#phone3type').val() !== "" && $('#phone3').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone3num', 'Phone type was given, number is required.');
+
+            window.ParsleyValidator
+                .addValidator('phone3type', function (value) {
+                    if ($('#phone3type').val() === "" && $('#phone3').val() === "") {
+                        return true;
+                    } else if ($('#phone3').val() !== "" && $('#phone3type').val() === "") {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }, 100)
+                .addMessage('en', 'phone3type', 'Phone number was given, type is required.');
+
+            window.ParsleyValidator
+                .addValidator('phonelength', function (value) {
+                    var valLength = value.split("_").join("").length;
+                    return valLength === 12 || valLength === 0;
+                }, 100)
+                .addMessage('en', 'phonelength', 'Please completely fill in this phone number.');
+
+            $j('#contact-form').parsley({
+                // bootstrap form classes
+                errorsWrapper: '<span class=\"help-block\" style="display: block;white-space: normal;word-wrap: break-word;"></span>',
+                errorTemplate: '<span class="error-message"></span>',
+                excluded: ':hidden'
             });
         }
     };
