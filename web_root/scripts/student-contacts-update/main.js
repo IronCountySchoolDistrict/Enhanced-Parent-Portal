8e2bb1a6c3917ea804ffba7b4a08ea80b4e940cb
Nathan Jones <nathan.jones@ironmail.org>
2015-01-02 10:27:02 -0700

    Added tooltip to app imports.

--- web_root/scripts/student-contacts-update/main.js
+++ web_root/scripts/student-contacts-update/main.js
@@ -7,16 +7,18 @@ require.config({
         service: '/scripts/student-contacts-update/service',
         actions: '/scripts/student-contacts-update/actions',
         config: '/scripts/student-contacts-update/config',
+        tooltip: '/scripts/student-contacts-update/tooltip',
 
         parsley: '//cdnjs.cloudflare.com/ajax/libs/parsley.js/2.0.5/parsley',
         underscore: 'underscore/underscore'
     }
 });
 
-require(['tableModule', 'actions'],
-    function (tableModule, actions) {
+require(['tableModule', 'actions', 'tooltip'],
+    function (tableModule, actions, tooltip) {
         'use strict';
         loadingDialogInstance.open();
         tableModule.main();
         actions.main();
+        tooltip.main();
     });
