6fe08b2b5434a747b54b55b3c04aca608c42c8c1
Nathan Jones <Purkinje90@gmail.com>
2015-05-14 13:55:34 -0600

    Merge pertinent changes from upstream 4.0.5 release.

--- web_root/admin/javascript/home.js
+++
@@ -1,6 +0,0 @@
-/*global $j*/
-$j(function() {
-    var template = $j($j('#enhancedpp-template').html());
-    var select = $j('a[href*="misc-district.html"]');
-    template.insertAfter(select);
-});
